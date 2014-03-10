09f7c89397efef2ffb4ba9618fe57cd37ebf2c9e
vtourraine <me@vtourraine.net>
2014-03-10 12:14:14 +0100

    Update copyright notice

--- MendeleySDK/MDLGroup.m
+++ MendeleySDK/MDLGroup.m
@@ -1,7 +1,7 @@
 //
 // MDLGroup.m
 //
-// Copyright (c) 2012-2013 shazino (shazino SAS), http://www.shazino.com/
+// Copyright (c) 2012-2014 shazino (shazino SAS), http://www.shazino.com/
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
