f3eb2c2803edd7f34cd132760d283abf52840fe7
vtourraine <me@vtourraine.net>
2015-11-06 12:31:58 +0100

    Update demo projects pods

---
+++ Examples/MendeleySDKDemoOSX/Pods/Headers/Public/MendeleySDK/MDLMendeleyAPIObject.h
@@ -0,0 +1,1 @@
+../../../../../../MendeleySDK/MDLMendeleyAPIObject.h
---
+++ Examples/MendeleySDKDemoOSX/Pods/Target Support Files/MendeleySDK/MendeleySDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MendeleySDK : NSObject
+@end
+@implementation PodsDummy_MendeleySDK
+@end
